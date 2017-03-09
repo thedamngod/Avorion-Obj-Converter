c592cf6814820b24b2edd76458d612b9e3f06d0d
Michael <michael.mutert@rwth-aachen.de>
2017-03-09 21:08:33 +0100

    Refactorings, Fixes

--- AvObjConverter.py
+++ AvObjConverter.py
@@ -8,6 +8,10 @@ from typing import List
 import numpy as np
 import sys
 
+import time
+
+DECIMALS = 8
+
 COLOR = 'color'
 UP = 'up'
 LOOK = 'look'
@@ -22,7 +26,6 @@ LOWER_X = 'lowerX'
 BLOCK = 'block'
 ITEM = 'item'
 
-
 Vertex = List[float]
 Face = List[int]
 Normal = List[float]
@@ -30,7 +33,8 @@ UV = List[float]
 
 
 class Block:
-    def __init__(self, index: int, x_min: float, x_max: float, y_min: float, y_max: float, z_min: float, z_max: float, type: int, material: int, look: int, up: int, color: str):
+    def __init__(self, index: int, x_min: float, x_max: float, y_min: float, y_max: float, z_min: float, z_max: float,
+                 type: int, material: int, look: int, up: int, color: str):
         self.material = float(material)
         self.x_max = float(x_max)
         self.y_min = float(y_min)
@@ -59,13 +63,14 @@ class AvToObjConverter:
             if os.path.exists(outputFile) and os.path.getsize(outputFile) > 0:
                 os.remove(outputFile)
                 print(outputFile)
-
+            start_time = time.time()
             file = open(outputFile, 'a')
             file.write("mtllib Avorion_Materials.mtl\n")
             for block in self.blocks:
                 file.write(self.convertBlockToObj(block))
             file.close()
-
+            print("Conversion process took {} seconds".format(time.time() - start_time))
+            print("Converted {} blocks".format(len(self.blocks)))
         except FileNotFoundError:
             print('Could not open output file')
 
@@ -75,12 +80,15 @@ class AvToObjConverter:
         print("Converting block with index {}, type = {}, look = {} and up = {}".format(
             block.index, block.type, block.look, block.up))
         faces = None
-        # vertex_normals = None
         v_indices = []
         f_indices = []
 
-        if block.type in range(1, 16) or block.type in range(50, 56) \
-                or block.type in [150, 170, 190, 510]:  # Non-diagonal blocks
+        if block.type in [10]:  # Hangar Block -- TODO: Replace Placeholder for hangar blocks
+            v_indices = [1, 2, 3, 4, 5, 6, 7, 8]
+            f_indices = [0, 1, 2, 3, 4, 5]
+
+        elif block.type in range(1, 16) or block.type in range(50, 56) \
+                or block.type in [150, 170, 190, 510]:  # Cuboid blocks
             v_indices = [1, 2, 3, 4, 5, 6, 7, 8]
             f_indices = [0, 1, 2, 3, 4, 5]
 
@@ -113,7 +121,6 @@ class AvToObjConverter:
 
         vertices, genfaces, vertex_normals = self.generate(v_indices, f_indices)
         vertices, vertex_normals = transformBlock(vertices, vertex_normals, block.look, block.up, block)
-        # vertex_normals = rotateVectors(vertex_normals, block.look, block.up)
         if faces is None:
             faces = genfaces
         out_string += self.generateObjectString(vertices, faces, vertex_normals, "{} {}".format(name, block.index))
@@ -142,10 +149,10 @@ class AvToObjConverter:
             [2, 6, 8],  # 6 Diagonal Right -
             [1, 5, 7],  # 7 Diagonal Left
             [1, 6, 5],  # 8 Diagonal Bottom
-            [4, 8, 7],  # 9 Diagonal Top -
+            [3, 7, 8],  # 9 Diagonal Top -
             [5, 6, 7],  # 10 Diagonal Back
-            [1, 2, 4],  # 11 Diagonal Front -
-            [2, 8, 4],  # 12 Triangle 7 Verts -
+            [1, 2, 3],  # 11 Diagonal Front -
+            [2, 3, 8],  # 12 Triangle 7 Verts -
             [1, 2, 7],  # 13 Triangle Front Bottom to Back Top Left
             [2, 6, 7],  # 14 Triangle Right Bottom to Back Top Left
             [1, 6, 7],  # 15 Triangle 4 Verts
@@ -160,16 +167,16 @@ class AvToObjConverter:
             [0, 0, 1],  # 4 Back
             [0, 0, -1],  # 5 Front
             [-1, 0, 0],  # 6 Diagonal Right
-            [1, 0, 0],  # Diagonal Left
-            [0, -1, 0],  # Diagonal Bottom
-            [0, 1, 0],  # Diagonal Top
-            [0, 0, 1],  # Diagonal Back
-            [0, 0, -1],  # Diagonal Front
-            [-1, -1, -1],  # Diagonal missing corner 4
-            [0, -1, -1],  # Triangle Front Bottom to Back Top Left
-            [-1, -1, 0],  # Triangle Right Bottom to Back Top Left
-            [-1, -1, -1],  # Triangle 4 Verts
-            [0, -1, -1]  # Slope
+            [1, 0, 0],  # 7 Diagonal Left
+            [0, -1, 0],  # 8 Diagonal Bottom
+            [0, 1, 0],  # 9 Diagonal Top
+            [0, 0, 1],  # 10 Diagonal Back
+            [0, 0, -1],  # 11 Diagonal Front
+            [-1, 1, -1],  # 12 Diagonal missing corner 4
+            [0, 1, -1],  # 13 Triangle Front Bottom to Back Top Left
+            [-1, 1, 0],  # 14 Triangle Right Bottom to Back Top Left
+            [-1, 1, -1],  # 15 Triangle 4 Verts
+            [0, 1, -1]  # 16 Slope
         ]
 
         out_vertices = [vertices[index - 1] for index in range(1, 9) if index in v_indices]  # Select Vertices
@@ -177,7 +184,8 @@ class AvToObjConverter:
         out_normals = []
         for face in out_faces:  # Select Normals
             index = faces.index(face)
-            out_normals.append(normalize(vertex_normals[index]))
+            # out_normals.append(normalize(vertex_normals[index]))
+            out_normals.append(vertex_normals[index])
 
         # Map the vertex indices to the correct number for all faces
         for i in range(len(v_indices)):
@@ -186,17 +194,14 @@ class AvToObjConverter:
                 for index in range(len(out_faces)):
                     out_faces[index] = list(map(mapping, out_faces[index]))
 
-        # Construct UV Maps
-        # for face in out_faces:
-        #     for vert in face:
-        #         pass
+        # Construct UV Maps -- TODO
 
         return out_vertices, out_faces, out_normals
 
     def generateObjectString(self, vertices: List[Vertex], faces: List[Face],
                              vertex_normals: List[Normal], name: str, uvs: List[UV] = None, smoothing: bool = False):
         out_string = "o {}\n".format(name)
-        smoothing = 1 if smoothing else 0
+        smoothing = "on" if smoothing else "off"
         for vertex in vertices:  # Add the strings for the vertices
             out_string += "v {} {} {}\n".format(vertex[0], vertex[1], vertex[2])
 
@@ -210,8 +215,7 @@ class AvToObjConverter:
         if vertex_normals is not None:  # Add the Strings for the vertex normals
             for normal in vertex_normals:
                 out_string += "vn "
-                for value in normal:
-                    out_string += "{} ".format(value)
+                out_string += "{} {} {}".format(normal[0], normal[1], normal[2])
                 out_string += "\n"
 
         out_string += "usemtl Standard\n"  # Add strign for the used material
@@ -233,6 +237,21 @@ class AvToObjConverter:
         return out_string
 
 
+def getSizes(block):
+    """
+    Calculates the maximum length of the block in the 3 directions.
+
+    :param block: The block which should be measured
+    :return: The measurements as a list [x,y,z]
+    """
+    sizes = [0, 0, 0]
+    if block is not None:
+        sizes[0] = (block.x_max - block.x_min) / 2
+        sizes[1] = (block.y_max - block.y_min) / 2
+        sizes[2] = (block.z_max - block.z_min) / 2
+    return sizes
+
+
 def normalize(vector: Vertex):
     length = 0
     for element in vector:
@@ -243,7 +262,17 @@ def normalize(vector: Vertex):
 
 
 def readSourceFile(file):
-    tree = xml.etree.ElementTree.parse(file).getroot()
+    """
+    Read the source file and return representations of the blocks as a list.
+
+    :param file: Path of the xml file which should be processed
+    :return: A list of blocks extracted from the file
+    """
+    try:
+        tree = xml.etree.ElementTree.parse(file).getroot()
+    except FileNotFoundError:
+        print("The input file was not found.\n Please make sure the file exists")
+        sys.exit()
     blocks = []
     for item in tree.findall(ITEM):
         for block in item.findall(BLOCK):
@@ -263,17 +292,17 @@ def readSourceFile(file):
     return blocks
 
 
-def rotateVectors(vectors: List[Vertex], look: int, up: int):
+def rotateVectors(vertices: List[Vertex], look: int = 5, up: int = 3):
     """
     Rotates the given vectors to correspond with the given values for look and up.
 
-    :param vectors: List of vectors to rotate
+    :param vertices: List of vectors to rotate
     :param look: Look value to orient the vectors to (Determines the face to the back)
     :param up: Up value to orient the vecturs to (Determines the face facing up)
     :return: The rotated vectors
     """
     return_vectors = []
-    for vector in vectors:
+    for vector in vertices:
         vector.append(1)
 
         temp_result = np.matrix(vector).transpose()
@@ -335,15 +364,25 @@ def rotateVectors(vectors: List[Vertex], look: int, up: int):
                 temp_result = rotate(temp_result, z_degrees=180)
         temp_result = temp_result.tolist()
         return_vectors.append(
-            [np.round(temp_result[0][0], 15), np.round(temp_result[1][0], 15), np.round(temp_result[2][0], 15)])
+            [np.round(temp_result[0][0], DECIMALS), np.round(temp_result[1][0], DECIMALS),
+             np.round(temp_result[2][0], DECIMALS)])
+        del (vector[-1])
     return return_vectors
 
 
-def transformBlock(verts: List[List[float]], vert_normals: List[List[float]], look: int, up: int, block=None):
+def transformBlock(verts: List[Vertex], vertex_normals: List[Normal] = None, look: int = 5, up: int = 3, block=None):
+    """
+    Transforms the given vertices
+
+    :param verts: Vertices which will be transformed
+    :param vertex_normals: Normal vectors which will be transformed
+    :param look: Index of the look vector
+    :param up: Index of the up vector
+    :param block: Block object, which is the base of the vertices and normals
+    :return: The transformed vertices and normal vectors
+    """
     vertices = verts.copy()
-    normals = vert_normals.copy()
-    output_vertices = []
-    output_normals = []
+    normals = vertex_normals.copy() if vertex_normals is not None else []
     translation_vector = [0, 0, 0]
     scale_amounts = [1, 1, 1]
 
@@ -351,57 +390,89 @@ def transformBlock(verts: List[List[float]], vert_normals: List[List[float]], lo
         translation_vector[0] = (block.x_max + block.x_min) / 2
         translation_vector[1] = (block.y_max + block.y_min) / 2
         translation_vector[2] = (block.z_max + block.z_min) / 2
-        scale_amounts[0] = (block.x_max - block.x_min) / 2
-        scale_amounts[1] = (block.y_max - block.y_min) / 2
-        scale_amounts[2] = (block.z_max - block.z_min) / 2
+        scale_amounts = getSizes(block)
 
     vertices = rotateVectors(vertices, look, up)
     normals = rotateVectors(normals, look, up)
 
-    for vertex in vertices:
-        vertex.append(1)
+    output_vertices = [transformVector(vertex, scale_amounts, translation_vector) for vertex in vertices]
 
-        temp_result = np.matrix(vertex).transpose()
-        temp_result = scale(temp_result, scale_amounts[0], scale_amounts[1], scale_amounts[2])
-        # temp_result = getTranslationMatrix(translation_vector[0], translation_vector[1],
-        #                                    translation_vector[2]) * temp_result
-        temp_result = translate(temp_result, translation_vector[0], translation_vector[1], translation_vector[2])
-
-        temp_result = temp_result.tolist()
-        result = [np.round(temp_result[0][0], 15), np.round(temp_result[1][0], 15), np.round(temp_result[2][0], 15)]
-        output_vertices.append(result)
-
-    for normal in normals:  # Update the normals to work with the scaling  -- Broken
-        normal.append(0)
-
-        temp_result = np.matrix(normal).transpose()
-        temp_result = scale(temp_result, scale_amounts[0], scale_amounts[1], scale_amounts[2])
-        temp_result = temp_result.tolist()
-        result = [np.round(temp_result[0][0], 15), np.round(temp_result[1][0], 15), np.round(temp_result[2][0], 15)]
-        output_normals.append(normalize(result))
+    scale_amounts = [1 / amount for amount in scale_amounts]
+    output_normals = [transformVector(normal, scale_amounts, [0, 0, 0]) for normal in normals]
 
     return output_vertices, output_normals
 
 
-def translate(vector, x, y, z):
+def transformVector(vector: Vertex, scales: List[float], translations: List[float]):
+    """
+    Scale the given vertex using the values from scales and afterwards translate the vertex using the translations vector.
+
+    :param vector: Vertex to transform
+    :param scales: Specifies the scale factors for the x, y, and z axis
+    :param translations: Specifies the translation amount in x, y, and z direction
+    :return: The transformed vector as Vertex
+    """
+    temp_vector = vector.copy()
+    temp_vector.append(1)
+    temp_result = np.matrix(temp_vector).transpose()
+
+    temp_result = scale(temp_result, scales[0], scales[1], scales[2])
+    temp_result = translate(temp_result, translations[0], translations[1], translations[2])
+
+    temp_result = temp_result.tolist()
+    result = [np.round(temp_result[0][0], DECIMALS), np.round(temp_result[1][0], DECIMALS),
+              np.round(temp_result[2][0], DECIMALS)]
+    # del (vector[-1])
+    return result
+
+
+def translate(vector: np.ndarray, x: float, y: float, z: float):
+    """
+    Translates the vector by the given amounts in x, y, and z direction
+
+    :param vector: The vector to translate
+    :param x: Translation distance in x direction
+    :param y: Translation distance in y direction
+    :param z: Translation distance in z direction
+    :return: The translated vector
+    """
     out_vector = _getTranslationMatrix(x, y, z).dot(vector)
     return out_vector
 
 
-def _getTranslationMatrix(x, y, z):
+def _getTranslationMatrix(x: float, y: float, z: float):
     return np.matrix([[1, 0, 0, x], [0, 1, 0, y], [0, 0, 1, z], [0, 0, 0, 1]])
 
 
-def scale(vector, x_amount=0, y_amount=0, z_amount=0):
-    xMatrix, yMatrix, zMatrix = np.identity(4), np.identity(4), np.identity(4)
-    xMatrix[0][0] = x_amount
-    yMatrix[1][1] = y_amount
-    yMatrix[2][2] = z_amount
-    result = xMatrix.dot(yMatrix.dot(zMatrix.dot(vector)))
+def scale(vector: np.ndarray, x_amount: float = 1, y_amount: float = 1, z_amount: float = 1, dim=4):
+    """
+    Scales the given vector in the given amounts
+
+    :param vector: The vector which will be scaled
+    :param x_amount: Scale in the x direction
+    :param y_amount: Scale in the y direction
+    :param z_amount: Scale in the z direction
+    :param dim: dimension of the vector
+    :return: The scaled vector
+    """
+    matrix = np.identity(dim)
+    matrix[0][0] = x_amount
+    matrix[1][1] = y_amount
+    matrix[2][2] = z_amount
+    result = matrix.dot(vector)
     return result
 
 
 def rotate(vector: np.ndarray, x_degrees: float = 0, y_degrees: float = 0, z_degrees: float = 0):
+    """
+    Rotates the given vector by the given amounts in the order z -> y -> x
+
+    :param vector: Vector to rotate
+    :param x_degrees: Rotation degree around the x-axis
+    :param y_degrees: Degrees around the y-axis
+    :param z_degrees: Degrees around the z-axis
+    :return: The rotated vector
+    """
     xMatrix, yMatrix, zMatrix = np.identity(4), np.identity(4), np.identity(4)
     if x_degrees != 0:
         c, s = np.cos(np.radians(x_degrees)), np.sin(np.radians(x_degrees))
@@ -424,16 +495,15 @@ def rotate(vector: np.ndarray, x_degrees: float = 0, y_degrees: float = 0, z_deg
     return result
 
 
-path = os.getenv('APPDATA') + '/Avorion/ships/'
-if len(argv) < 2:
-    print('Usage: AvObjConverter.py <file_name> [<path_to_folder_containing_file_name>]')
-    sys.exit()
-elif len(argv) == 3:
-    path = argv[2]
-ship_name = argv[1]
+if __name__ == "__main__":
+    path = os.path.join(os.getenv('APPDATA'), 'Avorion', 'ships')
+    if len(argv) < 2:
+        print("Please enter the name of the file you want to convert (without the file ending):")
+        ship_name = input("Name: ")
+    else:
+        ship_name = argv[1]
+        if len(argv) == 3:
+            path = argv[2]
 
-conv = AvToObjConverter()
-conv.convertToObj(path + ship_name + '.xml', './out/' + ship_name + '.obj')
-
-scale(np.matrix([1, 1, 1, 0]).transpose(), 5, 6, 7)
-rotateVectors([], None, None)
+    converter = AvToObjConverter()
+    converter.convertToObj(os.path.join(path, ship_name + '.xml'), './out/' + ship_name + '.obj')
